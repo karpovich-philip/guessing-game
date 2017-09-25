473ddabb4ffd92524053719f20e2e1ab2ad00269
Philip <philipkarpovich@gmail.com>
2017-09-25 21:04:16 +0300

    refactoring

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -8,25 +8,26 @@ class GuessingGame {
   }
 
   setRange(min, max) {
-    for (var i = min; i <= max; i++) {
-      var ar = this.state.array;
+    for (let i = min; i <= max; i++) {
+      let ar = this.state.array;
       ar.push(i);
     }
   }
 
   guess() {
-    var result = Math.ceil((this.state.array[1] + this.state.array[this.state.array.length - 1]) / 2);
-
-    this.state.result = result
-    return result;
+    let arr = this.state.array;
+    let result = Math.ceil((arr[1] + arr[arr.length-1])/2);
+    return this.state.result = result;
   }
 
   lower() {
-    this.state.array.splice(Math.ceil(this.state.array.length / 2), this.state.array.length - 1);
+    let arr = this.state.array;
+    arr.splice(Math.ceil(arr.length/2), arr.length-1);
   }
 
   greater() {
-    this.state.array.splice(0, Math.ceil(this.state.array.length / 2));
+    let arr = this.state.array;
+    arr.splice(0, Math.ceil(arr.length/2));
   }
 }
 
